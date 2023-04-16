88863fe21538d565d34b33f9c5e339275373a68b
LaryLvov <kolya.lvov@ukr.net>
2023-04-16 17:05:48 +0300

    Create main06_habr_gpt3.py
    Create file -sort folder-
     (dont work corectly )

---
+++ main06_habr_gpt3.py
@@ -0,0 +1,112 @@
+import os
+import argparse
+
+
+# main_path = "h:\\down"
+
+# key names will be folder names!
+extensions = {
+
+    'video': ['mp4', 'mov', 'avi', 'mkv', 'wmv', '3gp', '3g2', 'mpg', 'mpeg', 'm4v',
+              'h264', 'flv', 'rm', 'swf', 'vob'],
+
+    'data': ['sql', 'sqlite', 'sqlite3', 'csv', 'dat', 'db', 'log', 'mdb', 'sav',
+             'tar', 'xml'],
+
+    'audio': ['mp3', 'wav', 'ogg', 'flac', 'aif', 'mid', 'midi', 'mpa', 'wma', 'wpl',
+              'cda'],
+
+    'image': ['jpg', 'png', 'bmp', 'ai', 'psd', 'ico', 'jpeg', 'ps', 'svg', 'tif',
+              'tiff'],
+
+    'archive': ['zip', 'rar', '7z', 'z', 'gz', 'rpm', 'arj', 'pkg', 'deb'],
+
+    'text': ['pdf', 'txt', 'doc', 'docx', 'rtf', 'tex', 'wpd', 'odt'],
+
+    '3d': ['stl', 'obj', 'fbx', 'dae', '3ds', 'iges', 'step'],
+
+    'presentation': ['pptx', 'ppt', 'pps', 'key', 'odp'],
+
+    'spreadsheet': ['xlsx', 'xls', 'xlsm', 'ods'],
+
+    'font': ['otf', 'ttf', 'fon', 'fnt'],
+
+    'gif': ['gif'],
+
+    'exe': ['exe'],
+
+    'bat': ['bat'],
+
+    'apk': ['apk']
+}
+
+
+def get_subfolder_paths(folder_path):
+    subfolder_paths = [f.path for f in os.scandir(folder_path) if f.is_dir()]
+    return subfolder_paths
+
+
+def get_subfolder_names(folder_path):
+    subfolder_paths = get_subfolder_paths(folder_path)
+    subfolder_names = [os.path.basename(f) for f in subfolder_paths]
+    return subfolder_names
+
+
+def get_file_paths(folder_path):
+    file_paths = [f.path for f in os.scandir(folder_path) if f.is_file()]
+    return file_paths
+
+
+def create_folders_from_dict(extensions, folder_path):
+    for folder_name in extensions.keys():
+        os.makedirs(os.path.join(folder_path, folder_name), exist_ok=True)
+
+
+def sort_files(folder_path, extensions):
+    file_paths = get_file_paths(folder_path)
+    ext_list = list(extensions.items())
+
+    for file_path in file_paths:
+        extension = file_path.split('.')[-1]
+        file_name = os.path.basename(file_path)
+
+        for dict_key_int in range(len(ext_list)):
+            if extension in ext_list[dict_key_int][1]:
+                print(
+                    f'Moving {file_name} to {ext_list[dict_key_int][0]} folder\n')
+                os.rename(
+                    file_path, os.path.join(folder_path, ext_list[dict_key_int][0], file_name))
+                break
+
+
+def remove_empty_folders(folder_path):
+    subfolder_paths = get_subfolder_paths(folder_path)
+
+    for p in subfolder_paths:
+        if not os.listdir(p):
+            print('Deleting empty folder:', os.path.basename(p), '\n')
+            os.rmdir(p)
+
+
+def organize_files(folder_path, extensions):
+    create_folders_from_dict(extensions, folder_path)
+
+    subfolder_names = get_subfolder_names(folder_path)
+
+    for subfolder in subfolder_names:
+        subfolder_path = os.path.join(folder_path, subfolder)
+        sort_files(subfolder_path, extensions)
+        remove_empty_folders(subfolder_path)
+
+    sort_files(folder_path, extensions)
+    remove_empty_folders(folder_path)
+
+
+if __name__ == '__main__':
+    # folder_path = 'h:\\down'
+    # organize_files(folder_path)
+    parser = argparse.ArgumentParser(description='Organize files in a folder.')
+    parser.add_argument(
+        'folder_path', help='Path to the folder containing the files to be organized.')
+    args = parser.parse_args()
+    organize_files(args.folder_path, extensions)
